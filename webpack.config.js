2d49a8823d05037d1c080707d11c16c566a13c22
semantic.world <nikfakel@gmail.com>
2021-02-11 20:11:23 -0500

    Configure eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'the-game.bundle.js',
-    },
-    mode: 'development',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
-    plugins: [new HtmlWebpackPlugin()],
+  entry: './index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'the-game.bundle.js',
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin()],
 };
