917ce65a33e74e6d72cdee640e0b483145476547
semantic.world <nikfakel@gmail.com>
2021-02-11 20:46:40 -0500

    Configure TS

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './index.ts',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'the-game.bundle.js',
   },
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
