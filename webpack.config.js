ae1ee5f1ee895d5f9989046c465bb11e6821f943
semantic.world <nikfakel@gmail.com>
2021-02-11 19:40:01 -0500

    Add webpack, eslint, ts

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'the-game.bundle.js',
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [new HtmlWebpackPlugin()],
+};
